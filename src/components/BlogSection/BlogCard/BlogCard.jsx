817f6a131e141675d9617d58c83cb58d23909789
Masum Khandaker <masum.sde@gmail.com>
2022-01-25 13:25:02 +0600

    DEV #9 - pop up effect set up is done

--- src/components/BlogSection/BlogCard/BlogCard.jsx
+++ src/components/BlogSection/BlogCard/BlogCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Card, Col } from "react-bootstrap";
-
+import "./BlogCard.css";
 const BlogCard = ({ blogInfo }) => {
   const { image, title, date } = blogInfo;
   return (
-    <Col className="d-flex align-items-center justify-content-center">
+    <Col className="d-flex align-items-center justify-content-center blog-card">
       <Card style={{ width: "22rem" }}>
         <Card.Img variant="top" src={image} />
         <Card.Body>
