49481e9ccec7b0c0c0a101e8526eb3ee764bec41
Masum Khandaker <masum.sde@gmail.com>
2022-01-26 21:01:51 +0600

    FIX #19 - NeedOTO section responsiveness is done

--- src/components/NeedOTO/NeedOTO.jsx
+++ src/components/NeedOTO/NeedOTO.jsx
@@ -6,10 +6,12 @@ import { faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons";
 const NeedOTO = () => {
   return (
     <section className="need-oto">
-      <Container className="text-alignment">
-        <h2>Need OTO Applications?</h2>
+      <Container>
+        <h2 className="display-2">Need OTO Applications?</h2>
         <div className="d-flex align-items-center justify-content-start">
-          <a href="#contact">Contact With Us</a>
+          <a href="#contact" className="display-2">
+            Contact With Us
+          </a>
           <span>
             <FontAwesomeIcon icon={faLongArrowAltRight} size="2x" />
           </span>
