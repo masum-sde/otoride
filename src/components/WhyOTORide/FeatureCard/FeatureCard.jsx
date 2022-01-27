02a3228241b02f9508e5b1c3cf426ac6e215ec45
Masum Khandaker <masum.sde@gmail.com>
2022-01-27 23:47:13 +0600

    delete unnecessary stuff

--- src/components/WhyOTORide/FeatureCard/FeatureCard.jsx
+++ src/components/WhyOTORide/FeatureCard/FeatureCard.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import { Card, Col } from "react-bootstrap";
 import "./FeatureCard.css";
-import blogImage from "../../../image/blog_image.png";
 const FeatureCard = ({ feature }) => {
   const { icon, eclipse, title, description } = feature;
   return (
-    <>
-      <Col xs={12} sm={6} lg={3} className="d-flex align-items-center justify-content-center mt-5  ">
-        <Card className="feature-card">
-          <div id="image-container">
-            <img src={eclipse} alt="eclipse" id="eclipse" />
-            <img src={icon} alt="icon" id="icon" />
-          </div>
-          <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>{description}</Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-    </>
+    <Col xs={12} sm={6} lg={3} className="d-flex align-items-center justify-content-center mt-5 ">
+      <Card className="feature-card">
+        <div id="image-container">
+          <img src={eclipse} alt="eclipse" id="eclipse" />
+          <img src={icon} alt="icon" id="icon" />
+        </div>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 };
 
