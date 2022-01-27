261a379fb166e217b7b16555c161e55094c9d262
Masum Khandaker <masum.sde@gmail.com>
2022-01-28 00:46:02 +0600

    FIX #27 - Add back shadow in Blog card after hover

--- src/components/BlogSection/BlogCard/BlogCard.jsx
+++ src/components/BlogSection/BlogCard/BlogCard.jsx
@@ -4,9 +4,9 @@ import "./BlogCard.css";
 const BlogCard = ({ blogInfo }) => {
   const { image, title, date } = blogInfo;
   return (
-    <Col xs={12} sm={6} md={4} className="d-flex align-items-center justify-content-center blog-card">
-      <Card style={{ width: "22rem" }}>
-        <Card.Img variant="top" src={image} />
+    <Col xs={12} sm={6} md={4} className="d-flex align-items-center justify-content-center">
+      <Card style={{ width: "22rem" }} className="blog-card">
+        <Card.Img variant="top" src={image} className="image" />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text className="text-muted">{date}</Card.Text>
