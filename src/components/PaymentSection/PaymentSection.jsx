538db120b9433d4244bb790339ca6af2dd3d2abc
Masum Khandaker <masum.sde@gmail.com>
2022-01-28 12:41:08 +0600

    Payment Internet section responsive is done

---
+++ src/components/PaymentSection/PaymentSection.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./PaymentSection.css";
+import paymentImage from "../../image/payment.png";
+const PaymentSection = () => {
+  return (
+    <section className="payment-section">
+      <Container className="payment">
+        <Row className="d-flex align-items-center justify-content-center pb-5 pt-5">
+          <Col md={6} className="mt-3 mb-3">
+            <h1 className="display-4">A fully integrated suite of payments products</h1>
+          </Col>
+          <Col md={6} className="mt-3 mb-3">
+            <p className="text-muted">
+              Add different types of vehicles in the same app easily. Started with scooters, but want to add bikes? No
+              problem! We’ll help you
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <img src={paymentImage} alt="payment" className="img-fluid" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default PaymentSection;
